174a7611b194b970e58b22555aadcfdbdcbd32cf
Terry <terrybmochire@gmail.com>
2022-10-28 21:39:41 +0300

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/details', methods=['GET'])
+def get_details():
+    if request.method == 'GET':
+        return jsonify({"slackUsername": 'Keke',
+                        "backend": True,
+                        "age": 23,
+                        "bio": 'Software Engineer with '
+                               'focus on Back-End Web development and Android using Android and Java'})
