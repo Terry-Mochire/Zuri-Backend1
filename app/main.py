1968d7759616b8fe9ee9d9cc094ce5aafc60f42b
Terry <terrybmochire@gmail.com>
2022-10-28 22:28:30 +0300

    Add route for default

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
 @app.route('/details', methods=['GET'])
 def get_details():
     if request.method == 'GET':
